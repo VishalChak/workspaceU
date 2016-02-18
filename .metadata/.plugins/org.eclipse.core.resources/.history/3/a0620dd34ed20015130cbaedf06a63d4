e6d71c9756df850a1a889d4e5487f2bcee5e091b
VishalChak <vishalchak.nitk@gmail.com>
2016-02-18 13:21:53 +0530

    V

--- .metadata/.plugins/org.eclipse.core.resources/.history/3/a0620dd34ed20015130cbaedf06a63d4
+++
@@ -1,1 +0,0 @@
-public class String_demo { public static void main(String args[]) { char chars[] = {'a', 'b', 'c'}; String s = new String(chars); System.out.println(s); } }
--- Practice/src/power.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class power {
-	public static void main(String args[]){
-		Scanner scan = new Scanner(System.in);
-		int n = scan.nextInt();
-		System.out.println((n&-n)==n);
-	}
-}
